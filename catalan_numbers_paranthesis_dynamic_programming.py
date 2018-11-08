178f924f42de8b6db009d5e50db16d53b1d9d7b0
Sharky <shylaja.ch@gmail.com>
2018-11-08 13:13:22 -0800

    Create catalan_numbers_paranthesis_dynamic_programming.py
    Catalan numbers series 
    print all possible balanced parentheses of n pairs

---
+++ catalan_numbers_paranthesis_dynamic_programming.py
@@ -0,0 +1,31 @@
+ 
+def printParenthesis(str, n): 
+    if(n > 0): 
+        _printParenthesis(str, 0,  
+                          n, 0, 0); 
+    return; 
+  
+def _printParenthesis(str, pos, n,  
+                      open, close): 
+      
+    if(close == n): 
+        for i in str: 
+            print(i, end = ""); 
+        print(); 
+        return; 
+    else: 
+        if(open > close): 
+            str[pos] = '}';
+            #print("in first else loop", str )
+            _printParenthesis(str, pos + 1, n,  
+                              open, close + 1); 
+        if(open < n): 
+            str[pos] = '{'; 
+            #print ("in last else loop" , str)
+            _printParenthesis(str, pos + 1, n,  
+                              open + 1, close); 
+  
+# Driver Code 
+ n = 2;  
+.. str = [""] * 2 * n;  
+.. printParenthesis(str, n)
