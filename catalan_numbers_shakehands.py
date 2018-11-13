9b022e9ac8053007b2f70dc95b51b371303a365d
Sharky <shylaja.ch@gmail.com>
2018-11-13 06:46:41 -0800

    Update catalan_numbers_shakehands.py
    finalized shake hands algorithm that uses 2 arrays to match based on index

--- catalan_numbers_shakehands.py
+++ catalan_numbers_shakehands.py
@@ -1,28 +1,28 @@
 def printHands(a): 
     if(len(a) > 0 and len(a)%2 == 0): 
-      que = [""]*len(a)
-      _printStaircase(a, que,"",0,len(a)/2, 0, 0); 
+        op = [""]*(len(a)/2)
+        cl = [""]*(len(a)/2)
+        _printStaircase(a,0,op,cl,len(a)/2, 0, 0)
     return; 
   
-def _printStaircase(a, que,res,pos, n,open, close): 
-  if(close == n): 
-    print(res) 
-    return; 
-  else: 
-    tmp = list(que)
-    if(open > close): 
-      if(len(que) > 0):
-        c = que.pop()
-        res = res+c
-        res = res+a[pos:pos+1]
-        #print("in first else loop", str )
-        _printStaircase(a, que,res, pos +1, n,open, close + 1); 
+def _printStaircase(a, pos,op,cl, n,open, close): 
+    if(close == n): 
+        res = ""
+        for i in range(0,(len(a)/2)):
+            res = res + op[i]+cl[i]
+        print(res)
+        return
+    else: 
+        if(open > close): 
+            cl[close] = a[pos]
+            print("first else loop", op, cl )
+            _printStaircase(a, pos +1,op,cl, n,open, close + 1)
     if(open < n):
-      tmp.append(a[pos:pos+1])
-      #print ("in last else loop" , str)
-      _printStaircase(a, tmp,res, pos + 1, n,open + 1, close); 
+        op[open] = a[pos]
+        print ("last else loop" ,op, cl)
+        _printStaircase(a, pos + 1, op,cl,n,open + 1, close)
   
-# Driver Code 
-def main():
-  a = "abcd"
-  printHands(a)
+'''
+a = "abcd"
+printHands(a)
+'''
